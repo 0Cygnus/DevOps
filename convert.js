5688cbc3ad1aa72c2704630b583744eb71840539
0Cygnus <110432472+0Cygnus@users.noreply.github.com>
2022-08-03 18:27:07 -0300

    Delete convert.js

--- convert.js
+++
@@ -1,7 +0,0 @@
-exports.celsiusFahrenheit = (valor) => {
-    return Math.ceil(((valor * 9) / 5) + 32)
-};
-
-exports.fahrenheitCelsius = (valor) => {
-    return Math.ceil(((valor - 32) * 5) / 9);
-};
